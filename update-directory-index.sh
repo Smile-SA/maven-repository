0eed7edec1e567935eb462304925e0e0a3f05ffd
Romain Gonord <romain.gonord.opensource@neteyes.org>
2011-07-26 17:27:14 +0200

    documentation directory listing

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+RELEASES_PATH=./releases
+SNAPSHOTS_PATH=./snapshots
+DOC_PATH=./doc
 
-for DIR in $(find ./releases ./snapshots -type d); do
+for DIR in $(find ${RELEASES_PATH} ${SNAPSHOTS_PATH} -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
+
+(
+  echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+  ls -1p $DOC_PATH | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
+  echo -e "</pre>\n</body>\n</html>"
+) > "$DOC_PATH/index.html"
+
