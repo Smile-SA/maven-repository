211c3775c8ac52799bd083f79dc1ef73f915e619
Benoît SIMARD <github@bsimard.com>
2011-11-15 19:40:32 +0100

    running update-directory-index.sh script

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -2,8 +2,9 @@
 RELEASES_PATH=./releases
 SNAPSHOTS_PATH=./snapshots
 DOC_PATH=./doc
+RELEASES_ADDON_PATH=./releases-addon
 
-for DIR in $(find ${RELEASES_PATH} ${SNAPSHOTS_PATH} -type d); do
+for DIR in $(find ${RELEASES_PATH} ${SNAPSHOTS_PATH} ${RELEASES_ADDON_PATH} -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
